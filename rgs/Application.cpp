77afb960a0a9c95378f50fc2aaf3036cc830b732
Ysd97 <2234228201@qq.com>
2025-04-06 11:17:43 +0800

    update3

--- rgs/Application.cpp
+++ rgs/Application.cpp
@@ -2,34 +2,25 @@
 #include <string>
 
 namespace RGS {
-	class Application {
-	public:
-		Application(const std::string name, int width, int length) {
-			m_name = name;
-			m_width = width;
-			m_length = length;
-		}
-		~Application() {
+	Application::Application(const std::string name, int width, int length) : m_name(name), m_width(width), m_length(length) {
+		Init();
+	}
+	Application::~Application() {
 
+	}
+	void Application::Run() {
+		bool running = true;
+		if (running) {
+			OnUpdate();
 		}
-		void Run() {
-			bool running = true;
-			if (running) {
-				OnUpdate();
-			}
-		}
-	private:
-		void Init() {
+	}
+	void  Application::Init() {
 
-		}
-		void Terminate() {
+	}
+	void  Application::Terminate() {
 
-		}
-		void OnUpdate() {
+	}
+	void  Application::OnUpdate() {
 
-		}
-		std::string m_name;
-		int m_width;
-		int m_length;
-	};
+	}
 }
--- rgs/Main.cpp
+++ rgs/Main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "Application.h"
 using namespace std;
 
 int main() {
 	cout << "RGS" << endl;
+	RGS::Application app("RGS", 100, 200);
+	app.Run();
 	return 0;
 }
